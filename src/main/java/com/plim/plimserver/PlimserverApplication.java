d407e084eb9448d6936bb9b4cb4614ae420224bf
Jungwook Park <cco2416@gmail.com>
2021-03-25 18:57:37 +0900

    Test Maven

---
+++ src/main/java/com/plim/plimserver/PlimserverApplication.java
@@ -0,0 +1,13 @@
+package com.plim.plimserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlimserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlimserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plim/plimserver/PlimserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plim.plimserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlimserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
