52ff768062596d2b127e9dc7efb16aec891079f6
Jungwook Park <cco2416@gmail.com>
2021-03-28 23:50:01 +0900

    Demo test

--- src/main/java/com/plim/plimserver/Hello/controller/HelloController.java
+++ src/main/java/com/plim/plimserver/Hello/controller/HelloController.java
@@ -9,23 +9,23 @@ import java.time.LocalDate;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/demo")
     public Hello getHello(){
         return Hello.builder()
                 .name("PLIM")
-                .age(15)
+                .age(20)
                 .say("Hi")
                 .date(LocalDate.of(2021, 3,26))
                 .build();
     }
 
-    @GetMapping("/test")
-    public Hello getTest() {
-        return Hello.builder()
-                .name("PLIM")
-                .age(15)
-                .say("Hi")
-                .date(LocalDate.of(2021, 3,26))
-                .build();
-    }
+//    @GetMapping("/test")
+//    public Hello getTest() {
+//        return Hello.builder()
+//                .name("PLIM")
+//                .age(15)
+//                .say("Hi")
+//                .date(LocalDate.of(2021, 3,26))
+//                .build();
+//    }
 }
