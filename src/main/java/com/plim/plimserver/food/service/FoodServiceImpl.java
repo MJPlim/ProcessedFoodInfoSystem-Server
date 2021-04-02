4bc1790762b31ed8cafb121832c2a309da861b79
Jungwook Park <cco2416@gmail.com>
2021-04-02 14:52:46 +0900

    Add FoodServiceImpl's comment

--- src/main/java/com/plim/plimserver/food/service/FoodServiceImpl.java
+++ src/main/java/com/plim/plimserver/food/service/FoodServiceImpl.java
@@ -7,8 +7,6 @@ import com.plim.plimserver.food.api.ApiKey;
 import com.plim.plimserver.food.api.ApiKeyRepository;
 import com.plim.plimserver.food.dto.FoodDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -32,19 +30,19 @@ public class FoodServiceImpl implements FoodService{
 
 
     public ArrayList<FoodDTO> findFood(String foodName, int pageNo){
-        ArrayList<FoodDTO> foodList = new ArrayList<>();
-        int page = 1 + 5*(pageNo-1);
+        ArrayList<FoodDTO> foodList = new ArrayList<>(); // 결과를 담을 List
+        int page = 1 + 5*(pageNo-1); // 결과를 5개씩 받기 위한 리스트번호
 
         Random r = new Random();
         int id = r.nextInt(3)+1;
-        Optional<ApiKey> optionRawKey = this.apiKeyRepository.findById(id);
+        Optional<ApiKey> optionRawKey = this.apiKeyRepository.findById(id); // API Key를 랜덤하게 하여 key를 돌려써서 가져옴
         String rawMaterialKey = optionRawKey.orElseThrow(NoSuchElementException::new).getKeyValue();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        // RestTemplate 클래스를 이용하여 url, 받아올 정보의 타입을 인자로 넘겨서 반환된 json값을 지정한 타입으로 가져옴.
         String str = restTemplate.getForObject(rawMaterialURL + rawMaterialKey + "/" + this.apiCode
                 + "/json/" + page + "/" + (page+4) + "/PRDLST_NM=" + foodName, String.class);
 
+        //JsonParser를 이용하여 json의 구조 분리
         JsonParser jsonParser = new JsonParser();
         JsonObject obj = (JsonObject)((JsonObject) jsonParser.parse(str)).get(this.apiCode);// "C002" key의 value 가져오기
         Optional<JsonArray> optionalRow = Optional.ofNullable((JsonArray) obj.get("row"));// "row" key의 array 가져오기
