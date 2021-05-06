030ef7c020ae027cb73d4aa0562c393acf94f626
Harry-Bro <likeharrybro@gmail.com>
2021-05-06 14:43:07 +0900

    Remove reviewList and favoriteList

--- src/main/java/com/plim/plimserver/domain/food/dto/FoodDetailResponse.java
+++ src/main/java/com/plim/plimserver/domain/food/dto/FoodDetailResponse.java
@@ -1,13 +1,9 @@
 package com.plim.plimserver.domain.food.dto;
 
-import com.plim.plimserver.domain.favorite.domain.Favorite;
 import com.plim.plimserver.domain.food.domain.Food;
-import com.plim.plimserver.domain.review.domain.Review;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.List;
 
 @Getter
 @Builder
@@ -23,16 +19,13 @@ public class FoodDetailResponse {
     private final String nutrient;
     private final String allergyMaterials;
     private final Long viewCount;
-    private final List<Review> reviewList;
-    private final List<Favorite> favoriteList;
 
     public static FoodDetailResponse of(Food food) {
         return new FoodDetailResponse(
                 food.getId(), food.getFoodName(), food.getCategory(), food.getManufacturerName(),
                 food.getFoodImage().getFoodImageAddress(), food.getFoodImage().getFoodMeteImageAddress(),
                 food.getFoodDetail().getMaterials(), food.getFoodDetail().getNutrient(), food.getAllergyMaterials(),
-                food.getViewCount(), food.getReviewList(), food.getFavoriteList()
-        );
+                food.getViewCount());
     }
 
 }
