8fda513de617018d83dff407b7e53ca598364393
Harry-Bro <likeharrybro@gmail.com>
2021-05-07 19:48:41 +0900

    style: Remove white space

--- src/main/java/com/plim/plimserver/domain/user/api/UserController.java
+++ src/main/java/com/plim/plimserver/domain/user/api/UserController.java
@@ -52,8 +52,8 @@ public class UserController {
             , @Valid @RequestBody SetSecondEmailRequest request) {
         User user = this.userService.setSecondEmail(principal, request);
         return ResponseEntity.ok(SetSecondEmailResponse.builder()
-                                                       .email(user.getSecondEmail())
-                                                       .build());
+                .email(user.getSecondEmail())
+                .build());
     }
 
     @ApiOperation(value = "아이디 찾기", notes = "2차보안으로 설정한 이메일로 기존 이메일을 전송한다")
@@ -61,8 +61,8 @@ public class UserController {
     public ResponseEntity<FindEmailResponse> findEmail(@Valid @RequestBody FindEmailRequest request) {
         User user = this.userService.findEmail(request);
         return ResponseEntity.ok(FindEmailResponse.builder()
-                                                  .email(user.getSecondEmail())
-                                                  .build());
+                .email(user.getSecondEmail())
+                .build());
     }
 
     @ApiOperation(value = "패스워드 찾기", notes = "해당 메일 주소로 임시 패스워드를 전송한다.")
