2e89581bbe66453f2cf910f5b69e01064cfb9c0e
Jungwook Park <cco2416@gmail.com>
2021-05-12 16:06:39 +0900

    Fix compare algorithm

--- src/main/java/com/plim/plimserver/domain/food/util/SortByReviewRateAndDesc.java
+++ src/main/java/com/plim/plimserver/domain/food/util/SortByReviewRateAndDesc.java
@@ -2,17 +2,15 @@ package com.plim.plimserver.domain.food.util;
 
 import com.plim.plimserver.domain.food.domain.Food;
 import com.plim.plimserver.domain.review.domain.Review;
+import com.plim.plimserver.domain.review.domain.ReviewStateType;
 
 import java.util.Comparator;
 
 public class SortByReviewRateAndDesc implements Comparator<Food> {
     @Override
     public int compare(Food o1, Food o2) {
-        int o1Sum = o1.getReviewList().stream().mapToInt(Review::getReviewRating).sum();
-        int o2Sum = o2.getReviewList().stream().mapToInt(Review::getReviewRating).sum();
-        float o1Rate = o1Sum / (float)o1.getReviewList().size();
-        float o2Rate = o2Sum / (float)o2.getReviewList().size();
-
+        double o1Rate = o1.getReviewList().stream().filter(o -> o.getState().equals(ReviewStateType.NORMAL)).mapToInt(Review::getReviewRating).average().orElse(0);
+        double o2Rate = o2.getReviewList().stream().filter(o -> o.getState().equals(ReviewStateType.NORMAL)).mapToInt(Review::getReviewRating).average().orElse(0);
         if (o1Rate > o2Rate) {
             return -1;
         } else if (o1Rate < o2Rate) {
