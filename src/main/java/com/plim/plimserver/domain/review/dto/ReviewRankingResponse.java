ef4ea7cffe3d13f6c65387c555042a497c31e1a9
Chanho Jung <cksgh3422@gmail.com>
2021-05-28 14:54:50 +0900

    Fixed readReviewRanking and reviewLikeCheck

--- src/main/java/com/plim/plimserver/domain/review/dto/ReviewRankingResponse.java
+++ src/main/java/com/plim/plimserver/domain/review/dto/ReviewRankingResponse.java
@@ -14,12 +14,19 @@ public class ReviewRankingResponse {
 	private String foodName;
 	
 	private String avgRating;
+	
+	private String category;
+	
+	private String foodImageAddress;
 
 	public static ReviewRankingResponse from(ReviewSummary reviewSummary) {
+		String[] splitCategory = reviewSummary.getFood().getCategory().split("_");
 		return ReviewRankingResponse.builder()
 				.foodId(reviewSummary.getFood().getId())
 				.foodName(reviewSummary.getFood().getFoodName())
 				.avgRating(String.format("%.02f", reviewSummary.getAvgRating()))
+				.category(splitCategory[0])
+				.foodImageAddress(reviewSummary.getFood().getFoodImage().getFoodImageAddress())
 				.build();
 	}
 
