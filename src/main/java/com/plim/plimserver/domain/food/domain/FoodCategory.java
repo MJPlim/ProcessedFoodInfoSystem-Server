c8a8e1546d7bd5546b8a0d715a67b2d944eaad97
Jungwook Park <cco2416@gmail.com>
2021-06-04 19:56:37 +0900

    Fix food category

--- src/main/java/com/plim/plimserver/domain/food/domain/FoodCategory.java
+++ src/main/java/com/plim/plimserver/domain/food/domain/FoodCategory.java
@@ -8,12 +8,14 @@ import lombok.RequiredArgsConstructor;
 public enum FoodCategory {
 
     SNACK("간식"),
-    TEA("음료/차"),
+    DRINK("음료/차"),
     DAIRY("유제품"),
+    MEAT("육류"),
     AGRICULTURAL_FISHERY("농수산물"),
     KIMCHI("김치"),
     SEASONING("조미료"),
     INSTANT("즉석조리식품"),
+    MATERIAL("식재료"),
     ETC("기타");
 
     private final String message;
